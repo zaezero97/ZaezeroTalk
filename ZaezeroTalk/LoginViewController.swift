c9bf5320cb4ab61e1c1be7bda1217de933fc32e7
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-07 01:18:19 +0900

    add splash screen
    2021 - 11 - 7

---
+++ ZaezeroTalk/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  ZaezeroTalk
+//
+//  Created by 재영신 on 2021/11/07.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
--- ZaezeroTalk/ViewController.swift
+++ ZaezeroTalk/ViewController.swift
@@ -13,22 +13,24 @@ import FirebaseRemoteConfig
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         let remoteConfig = RemoteConfig.remoteConfig()
+        
         remoteConfig.setDefaults(fromPlist: "remoteConfigDefault")
-        remoteConfig.fetch(withExpirationDuration: 3600) { (status,error) in
+        remoteConfig.fetch(withExpirationDuration: 3600) { (status,error) in // fetch하면 값을 서버에서 값을 가져오고 캐시한다. activate 하면 가장 최근에 가져온(캐시 된) 설정을 활성화(디폴트 config나 가져온 remoteconfig -> active config (로컬데이터로 변환하는 과정).
+            // 지정한 시간동안은 서버에서 가져오는 것이 아닌 캐시된 값을꺼내는 것이므로 그동안은 변경된 값을 가져오지 못하는 것 같다.
             if status == .success{
                 print("Config fetched!")
-                remoteConfig.activate { (change, error) in
-                    print(change)
-                    print(remoteConfig["app_color"].stringValue)
+                remoteConfig.activate { (change, error) in //활성화 함수 함수를 호출하지 않으면 가져온 값이 적용이 되지 않는 거 같다.
+                    print("Change : \(change)")
                 }
+
             }else{
                 print("Config not fetched")
                 print("Error: \(error?.localizedDescription ?? "No error available.")")
             }
-            
+            print(remoteConfig["app_color"].stringValue)
         }
+       
     }
 }
 
