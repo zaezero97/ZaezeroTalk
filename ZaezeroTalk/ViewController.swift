0a33c3a905a83861351943195e3ac691bbee0b5e
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-06 14:24:33 +0900

    add code that fetch remote config at Firebase
    2021 - 11 - 6

--- ZaezeroTalk/ViewController.swift
+++ ZaezeroTalk/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 import SnapKit
 import Firebase
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
+        let remoteConfig = RemoteConfig.remoteConfig()
+        remoteConfig.setDefaults(fromPlist: "remoteConfigDefault")
+        remoteConfig.fetch(withExpirationDuration: 3600) { (status,error) in
+            if status == .success{
+                print("Config fetched!")
+                remoteConfig.activate { (change, error) in
+                    print(change)
+                    print(remoteConfig["app_color"].stringValue)
+                }
+            }else{
+                print("Config not fetched")
+                print("Error: \(error?.localizedDescription ?? "No error available.")")
+            }
+            
+        }
     }
 }
 
