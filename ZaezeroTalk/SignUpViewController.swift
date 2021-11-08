eb1eac60194991cba790431b11447237647f47f7
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-08 10:41:01 +0900

    SignUp page 래이아웃 디자인
    2021 - 11 -8

--- ZaezeroTalk/SignUpViewController.swift
+++ ZaezeroTalk/SignUpViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+    @IBOutlet weak var pwdCheckTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var emailErrorLabel: UILabel!
+    @IBOutlet weak var nameErrorLabel: UILabel!
+    @IBOutlet weak var pwdErrorLabel: UILabel!
+    @IBOutlet weak var pwdCheckErrorLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-
 }
+
+
